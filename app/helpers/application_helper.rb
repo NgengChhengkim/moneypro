fc69f0b6e4beb25438a7820c9845314e2ccb4e23
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-09-04 15:27:21 +0700

    design_layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def flash_class level
+    case level
+    when :notice then "alert-info"
+    when :error then "alert-error"
+    when :alert then "alert-warning"
+    when :success then "alert-success"
+    end
+  end
+
+  def flash_message flash_type
+    t "flashs.messages.#{flash_type}", model_name: controller_name.classify
+  end
 end
