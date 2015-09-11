0f6df3076a573dc7877c1998b65e4e4c25e6d187
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-09-11 10:46:07 +0700

    user-expense

--- app/models/user_expense.rb
+++ app/models/user_expense.rb
@@ -1,9 +1,10 @@
 class UserExpense < ActiveRecord::Base
+  paginates_per Settings.pagination.per_page
   belongs_to :expense_category
   belongs_to :payment_method
   belongs_to :user
 
-  validates :amount, presence: true, numericality: greater_than: 0
+  validates :amount, presence: true, numericality: {greater_than: 0}
   validates :date, presence: true
 
   delegate :name, to: :expense_category, prefix: true, allow_nil: true
