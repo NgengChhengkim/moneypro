559df1d1fd1f9854f6fb8cd9c5719818539a83fd
Ngeng Chhengkim <chhengkimngeng@gmail.com>
2015-11-09 08:54:41 +0700

    make data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Rake::Task["db:migrate:reset"].invoke
+
+User.create name: "Admin",
+            email: "admin@gmail.com",
+            password: 123456,
+            password_confirmation: 123456,
+            admin: 1
+
+user = User.create name: "User",
+            email: "user@gmail.com",
+            password: 123456,
+            password_confirmation: 123456,
+            admin: 0
+
+payment_methods = ["Cash", "Check", "Visa Card"]
+payment_methods.each do |payment_method|
+  PaymentMethod.create name: payment_method,
+                       description: payment_method,
+                       initial_balance: [*100..1000].sample,
+                       user_id: 2
+end
+
+income_categories = ["Deposits", "Salary", "Saving"]
+income_categories.each do |income_category|
+  IncomeCategory.create name: income_category,
+                       description: income_category,
+                       user_id: 2
+end
+
+expense_categories = ["Car", "Cloth", "Food","Heallth", "House"]
+expense_categories.each do |expense_category|
+  ExpenseCategory.create name: expense_category,
+                         description: expense_category,
+                         user_id: 2
+end
+
+date = [2.day.ago, 1.day.ago, Date.today]
+10.times do
+  UserIncome.create amount: [*100..1000].sample,
+                    description: "description",
+                    date: date.sample,
+                    user_id: 2,
+                    income_category: IncomeCategory.all.sample,
+                    payment_method: PaymentMethod.all.sample
+end
+
+10.times do
+  UserExpense.create amount: [*100..1000].sample,
+                    description: "description",
+                    date: date.sample,
+                    user_id: 2,
+                    expense_category: ExpenseCategory.all.sample,
+                    payment_method: PaymentMethod.all.sample
+end
